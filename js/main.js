2d74d769258125dc0e17458d811546743ff319f7
DESKTOP-G69CNPE\nguyen tu <nguyentued@gmail.com>
2018-09-20 13:48:09 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+    myFunction()
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky + 1) {
+
+        navbar.classList.add("sticky");
+        //set z-index to hover other component
+        navbar.style.zIndex = "1";
+        navbar.classList.add("border-bottom");
+    } else {
+        navbar.classList.remove("sticky");
+        navbar.classList.remove("border-bottom");
+    }
+}
