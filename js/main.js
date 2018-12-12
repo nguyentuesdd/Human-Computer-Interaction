f22a425476c141fc497d5b4fed75913eb2dd9dad
DESKTOP-G69CNPE\nguyen tu <nguyentued@gmail.com>
2018-12-12 21:52:52 +0700

    fix error js file

--- js/main.js
+++ js/main.js
@@ -23,3 +23,10 @@ function myFunction() {
         navbar.classList.remove("shadow");
     }
 }
+$(document).ready(function(){
+	$('.collapse').on('shown.bs.collapse', function(){
+		$(this).parent().find(".fa-plus-circle").removeClass("fa-plus-circle").addClass("fa-minus-circle");
+	}).on('hidden.bs.collapse', function(){
+		$(this).parent().find(".fa-minus-circle").removeClass("fa-minus-circle").addClass("fa-plus-circle");
+	});
+});
